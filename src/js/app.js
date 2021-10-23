fabf73fe49643cad310fa4a528893a446e90ca44
ropi <14427@utcv.edu.mx>
2021-10-23 09:14:04 -0500

    Actualizando serviceWoker

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,14 @@ let MAIN;
 let MODAL_POST;
 let BTN_SHOW_POST;
 let BTN_CANCEL_POST;
+let deferredPrompt;
+
+//Anular el efecto del mensaje de instalación de la app
+window.addEventListener('beforeinstallprompt', (e) => {
+  console.log('anulando mensaje');
+  e.preventDefault();
+  deferredPrompt = e;
+});
 
 // Cuando se cargue todo nuestro DOM
 window.addEventListener('load', async () => {
@@ -20,6 +28,19 @@ window.addEventListener('load', async () => {
         console.log('Service worker registrado');
       }
     }
+
+    //añadir el evento a la instalación
+
+const bannerInstall = document.querySelector('#banner-install');
+bannerInstall.addEventListener('click', async () => {
+  if (deferredPrompt) {
+    deferredPrompt.prompt();
+    const response = await deferredPrompt.userChoice;
+    if (response.outcome === 'dismissed') {
+      console.error('El usuario cancelo la instalación');
+    }
+  }
+}); 
   });
 
   const showPostModal = () => {
@@ -33,4 +54,5 @@ window.addEventListener('load', async () => {
     MAIN.style.display = 'block';
     MODAL_POST.style.transform = 'translateY(100vh)';
   };
+
 
